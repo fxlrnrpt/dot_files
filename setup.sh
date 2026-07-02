a6c96b37e23fd422c45cbf2d522b2ff8b3829a9c
Andrey Goncharov <andrey@faillearnrepeat.net>
2026-07-02 17:39:38 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
 ln -sf "$PWD/.aerospace.toml" "$HOME/.aerospace.toml"
+
+APPSUPPORT_DIR="$HOME/Library/Application Support/com.mitchellh.ghostty"
+mkdir -p "$APPSUPPORT_DIR"
+ln -sfn "$PWD/ghostty/config" "$APPSUPPORT_DIR/config"
